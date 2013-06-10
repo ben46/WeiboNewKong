315b82897bdb0cd02198ae9ff5980c91d75f0fe1
ben46 <ben02060846@gmail.com>
2013-06-10 08:32:12 +0800

    本想修改相册的悬浮。

--- Weibo.user.js
+++ Weibo.user.js
@@ -24,11 +24,28 @@ var DATE = chrome.i18n.getMessage("appReleaseDate");
 /*popup images*/
 function hoverimg()
 {
+
+	$(".photo").each( function (){
+		$(this).hover(function(){
+			var x = event.clientX + document.body.scrollLeft;
+			var y = event.clientY + document.body.scrollTop;
+			$("#kong_hover_img").attr( "src", $(this).attr( "src" ).replace( "mw205", "mw690"  ) );
+			$("#kong_hover_img").css( "top", "40px" );
+			$("#kong_hover_img").css( "right", "100px" );
+			$("#kong_hover_img").css( "position", "fixed" );
+			$("#kong_hover_img").css( "border", "3px solid white" );
+			$("#kong_hover_img").show();
+		},function(){
+			$("#kong_hover_img").hide();
+		});
+	});
+
+
 	$(".bigcursor").each( function (){
 		$(this).hover(function(){
 			var x = event.clientX + document.body.scrollLeft;
 			var y = event.clientY + document.body.scrollTop;
-			$("#kong_hover_img").attr( "src", $(this).attr( "src" ).replace( "thumbnail", "bmiddle"  ) );
+			$("#kong_hover_img").attr( "src", $(this).attr( "src" ).replace( "thumbnail", "bmiddle"  ).replace( "square", "bmiddle"  ) );
 			$("#kong_hover_img").css( "top", "40px" );
 			$("#kong_hover_img").css( "right", "100px" );
 			$("#kong_hover_img").css( "position", "fixed" );
@@ -89,15 +106,15 @@ function doit(options) {
 		 $(document).attr('title').match("发出的评论") || $(document).attr('title').match("我的評論") ||
 		 $(document).attr('title').match("我的私信") || $(document).attr('title').match("我的私人訊息") ||
 		 $(document).attr('title').match("我的收藏") || $(document).attr('title').match("我的收藏") ||
-		  $(document).attr('title').match("新浪微博") ||
+		 $(document).attr('title').match("新浪微博") 
 	){
 		$("<style type='text/css'> .kong_button_original { color:" + $('.MIB_linkbl > a').css("color") + "; } </style>").appendTo("head");
 		$("<div><img id=\"kong_hover_img\" /></div>").appendTo("body");
 		hoverimg();
 	}
-	//TODO Search page
 
 
+}
 
 
 //get settings
